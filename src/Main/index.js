15dc2e53095f89ee636c2805f15e8c721a45bd91
megalosghb <ebmail@rambler.ru>
2020-06-25 21:14:23 +0200

    initial commit

---
+++ src/Main/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Todos } from "../Todos";
+export const Main = () => {
+  return (
+    <main className="App-main">
+      <Todos />
+    </main>
+  );
+};
---
+++ src/Todos/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+export const Todos = () => {
+  return (
+    <header className="Todo">
+      <ul>
+        <li>Lern</li>
+        <li>Run</li>
+        <li>Shopping</li>
+      </ul>
+    </header>
+  );
+};
