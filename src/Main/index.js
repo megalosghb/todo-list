fbc14b7df073e6600eacf31078b5adb6d69c9da4
megalosghb <ebmail@rambler.ru>
2020-06-28 21:41:34 +0200

    iteration

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,9 +1,28 @@
-import React from "react";
-import { Todos } from "../Todos";
+import React from 'react';
+import { Todos } from '../Todos';
 export const Main = () => {
+  const [todos, setTodos] = React.useState([
+    {
+      id: 0,
+      title: 'Lern',
+      done: false,
+    },
+
+    {
+      id: 1,
+      title: 'Run',
+      done: false,
+    },
+
+    {
+      id: 2,
+      title: 'Shopping',
+      done: false,
+    },
+  ]);
   return (
     <main className="App-main">
-      <Todos />
+      <Todos todos={todos} />
     </main>
   );
 };
