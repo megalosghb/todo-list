b01929983fcf988b5c573cf5bbd09f327cd677a1
megalosghb <ebmail@rambler.ru>
2020-07-16 01:52:01 +0200

    header making with material ui

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from '@material-ui/core';
 
 export const Header = () => {
   return (
     <header className="App-header">
-      <nav>
-        <ul>
-          <li>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={Header.menuButton}
+            color="inherit"
+            aria-label="menu"
+          ></IconButton>
+          <Typography variant="h6" className={Header.title}>
+            HEADER
+          </Typography>
+          <Button color="inherit">
             <Link to="/">Home</Link>
-          </li>
-          <li>
+          </Button>
+          <Button color="inherit">
             <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+          </Button>
+        </Toolbar>
+      </AppBar>
     </header>
   );
 };
