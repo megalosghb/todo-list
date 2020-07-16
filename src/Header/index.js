75dbbff6398d3d382efe184e97bc03de1e699bcb
megalosghb <ebmail@rambler.ru>
2020-07-16 18:28:24 +0200

    delete 13th line and change Button

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import {
   AppBar,
   Toolbar,
@@ -7,29 +6,25 @@ import {
   Typography,
   Button,
 } from '@material-ui/core';
+import { About } from '../About';
 
 export const Header = () => {
   return (
-    <header className="App-header">
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={Header.menuButton}
-            color="inherit"
-            aria-label="menu"
-          ></IconButton>
-          <Typography variant="h6" className={Header.title}>
-            HEADER
-          </Typography>
-          <Button color="inherit">
-            <Link to="/">Home</Link>
-          </Button>
-          <Button color="inherit">
-            <Link to="/about">About</Link>
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </header>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={Header.menuButton}
+          color="inherit"
+          aria-label="menu"
+        ></IconButton>
+        <Typography variant="h6" className={Header.title}>
+          HEADER
+        </Typography>
+        <Button color="inherit">
+          <Button to={About} component={About} />
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 };
