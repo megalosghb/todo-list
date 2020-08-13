0358e85dd64c0afab52ba15b9006c73602fcad56
megalosghb <ebmail@rambler.ru>
2020-08-13 20:58:04 +0200

    delete-function

--- src/Todos/index.js
+++ src/Todos/index.js
@@ -1,34 +1,47 @@
 import React from 'react';
-import { FormControlLabel } from '@material-ui/core';
+import {
+  FormControlLabel,
+  List,
+  ListItem,
+  ListItemSecondaryAction,
+  IconButton,
+} from '@material-ui/core';
 import { Checkbox } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 import './style.css';
 
-export const Todos = ({ showAll, todos, changeItem }) => {
-  const handleChange = (id) => {
-    changeItem(id);
-  };
-
+export const Todos = ({ showAll, todos, changeItem, deleteItem }) => {
   return (
-    <ul className="todos">
+    <List className="todos">
       {todos.map((todo) => {
         if (!showAll && todo.done) {
           return null;
         }
         return (
-          <li key={todo._id}>
+          <ListItem key={todo._id}>
             <FormControlLabel
               control={
                 <Checkbox
                   checked={todo.done}
                   color="primary"
-                  onChange={() => handleChange(todo._id)}
+                  onChange={() => changeItem(todo._id)}
                 />
               }
               label={todo.title}
             />
-          </li>
+            <ListItemSecondaryAction>
+              <IconButton
+                edge="end"
+                aria-label="comments"
+                color="secondary"
+                onClick={() => deleteItem(todo._id)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
         );
       })}
-    </ul>
+    </List>
   );
 };
